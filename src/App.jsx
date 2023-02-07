b3959f00a7dcc536d44061244d80d8ddeecd4259
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-02-07 18:46:44 +0600

    Vetka Rusya

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello everyone! It is time to wake up! You facking bulshits we will do our jetim project! Siiuuuu!</h1>
+      <h1>vetka Rusya</h1>
     </div>
   );
 }
