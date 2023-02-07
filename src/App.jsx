6d11eaf506fac0ed973f20f815cdc3b9e0d37ff9
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-02-07 18:39:36 +0600

    project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello everyone! It is time to wake up! You facking bulshits we will do our jetim project! Siiuuuu!</h1>
+    </div>
+  );
+}
+
+export default App;
