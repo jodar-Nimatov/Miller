23b1c08a2560ea64f321d7b349ee97af431f0b37
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-02-11 17:27:17 +0300

    Последнее изменения для desktop

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -6,12 +6,20 @@ import basketlogo from "../../assets/Vector (1).svg"
 import userlogo from "../../assets/Vector (2).svg"
 
 const Header = () => {
+  // const getSearcher = () => {
+  //   return (
+  //     <div class="search">
+  //       <input type="text" class="search-field" placeholder="Введите поисковый запрос или URL"/>
+  //       <img src={searchlogo} alt="" class="search-icon"/>
+  //     </div>
+  //   )
+  // }
   return (
     <div className="header">
       <div className="container">
         <div className="header-inner">
           <div className="header-logo">
-            <img src={millerLogo} alt=""/>
+            <img src={millerLogo} width='260px' height='116px' alt="" />
           </div>
           <div className="header-menu">
             <ul>
@@ -21,9 +29,15 @@ const Header = () => {
             </ul>
           </div>
           <div className="header-icons">
-            <img src={searchlogo} alt="" />
-            <img src={basketlogo} alt="" />
-            <img src={userlogo} alt="" />
+            <div className="search-icon">
+              <img src={searchlogo} alt="" className='header-searchIcon header-icon' />
+            </div>
+            <div className="basket-icon">
+              <img src={basketlogo} alt="" className='header-basketIcon header-icon'/>
+            </div>
+            <div className="user-icon">
+              <img src={userlogo} alt="" className='header-userIcon header-icon'/>
+            </div>
           </div>
         </div>
       </div>
