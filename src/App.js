bc87407f8874b57077b16076aa891a9075679d9d
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-02-16 18:40:15 +0600

    jkflsad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import './scss/style.scss'
 import Layout from './Layout/Layout';
 import VendingItems from './pages/Catalog/Vending/Vending-Items/Vending-Items';
 import HealthyEatingItems from './pages/Catalog/HealthyEating/HealthyEating-Items/HealthyEating-Items';
@@ -10,6 +9,7 @@ import TeaItems from './pages/Catalog/Tea/Tea-Items/Tea-Items';
 import Contacts from './pages/Contacts/Contacts';
 import Blog from './pages/Blog/Blog';
 import ClipLoader from 'react-spinners/ClipLoader'
+import './scss/style.scss'
 
 
 function App() {
@@ -23,8 +23,8 @@ function App() {
       <Route path='/' element={<Layout/>}>
         <Route path='' element={<Home/>}/>
         <Route path='/coffee' element={<CoffeeItems/>}/>
-        <Route path='/blog' element={<Blog/>}/>
         <Route path='/tea' element={<TeaItems/>}/>
+        <Route path='/blog' element={<Blog/>}/>
         <Route path='/wending' element={<VendingItems/>}/>
         <Route path='/healthy' element={<HealthyEatingItems/>}/>
         <Route path='/contacts' element={<Contacts/>}/>
