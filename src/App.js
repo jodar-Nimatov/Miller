8daf20cf16ba76a1f254134d6b7818af47119ebf
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-02-18 18:23:20 +0600

    my vetka

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from 'react'
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Layout from "./Layout/Layout";
-import VendingItems from "./pages/Catalog/Vending/Vending-Items/Vending-Items";
-import HealthyEatingItems from "./pages/Catalog/HealthyEating/HealthyEating-Items/HealthyEating-Items";
+import Layout from './Layout/Layout';
+import VendingItems from './pages/Catalog/Vending/Vending-Items/Vending-Items';
+import HealthyEatingItems from './pages/Catalog/HealthyEating/HealthyEating-Items/HealthyEating-Items';
 import CoffeeItems from "./pages/Catalog/Coffee/Coffee-Items/Coffee-Items";
 import TeaItems from './pages/Catalog/Tea/Tea-Items/Tea-Items';
 import Contacts from './pages/Contacts/Contacts';
@@ -11,14 +11,9 @@ import Blog from './pages/Blog/Blog';
 import Profile from './pages/Profile/Profile'
 import ClipLoader from 'react-spinners/ClipLoader'
 
-import './scss/style.scss'
-import Layolog from './pages/Catalog/Layolog';
-
-
-
 function App() {
-  const [loading, setLoading] = useState(false);
-  document.addEventListener("DOMContentLoaded", () => setLoading(true));
+  const [loading, setLoading] = useState(false)
+  document.addEventListener('DOMContentLoaded', ()=>setLoading(true))
   return (
   <>
   {
@@ -26,16 +21,13 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route path='' element={<Home/>}/>
+        <Route path='/coffee' element={<CoffeeItems/>}/>
+        <Route path='/tea' element={<TeaItems/>}/>
         <Route path='/blog' element={<Blog/>}/>
+        <Route path='/wending' element={<VendingItems/>}/>
+        <Route path='/healthy' element={<HealthyEatingItems/>}/>
         <Route path='/contacts' element={<Contacts/>}/>
         <Route path='/Profile' element={<Profile  />}/>
-        <Route path='/catalog' element={<Layolog/>}/>
-        {/* Каталог товаров */}
-        <Route path='/catalog/coffee' element={<CoffeeItems/>}/>
-        <Route path='/catalog/tea' element={<TeaItems/>}/>
-        <Route path='/catalog/wanding' element={<VendingItems/>}/>
-        <Route path='/catalog/healthy' element={<HealthyEatingItems/>}/>
-        {/* Каталог товаров */}
       </Route>
     </Routes>
   }
