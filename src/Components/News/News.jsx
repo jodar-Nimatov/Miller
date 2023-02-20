2e86c43ed00bae19373e117af21c0b1912ccfb09
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-02-20 15:54:58 +0600

    add: jodar

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,9 +1,7 @@
-import React, {useEffect, useState} from 'react'
+import { React, useEffect, useState } from 'react';
 
 const News = () => {
-
   const [news, setNews] = useState({data: []})
-
 
   useEffect(() => {
     fetch(`http://localhost:3333/news-posts`)
@@ -18,31 +16,29 @@ const News = () => {
 
   return (
     <div className='news'>
-        <div className="news-container">
-            <div className="news-inner">
-                <h2>Новости компании</h2>
-                <div className="news-posts-row">
-                    {
-                      posts.map(item => {
-                        return (
-                          <div className="post-item">
-                            <div className="post-item-left">
-                              <img className='post-img' src={item.image} alt="" />
-                            </div>
-                            <div className="post-right">
-                              <h1 className='post-title'>{item.title}</h1>
-                              <p className='post-desc'>{item.desc}</p>
-                            </div>
-                          </div>
-                        )
-                      })
-                    }
-                </div>
-            </div>
+      <div className="news-container">
+        <div className="news-inner">
+          <h2>Новости компании</h2>
+          <div className="news-posts-row">
+            {
+              posts.map(item => (
+                  <div className="post-item">
+                    <div className="post-item-left">
+                      <img className='post-img' src={item.image} alt="" />
+                    </div>
+                    <div className="post-right">
+                      <h1 className='post-title'>{item.title}</h1>
+                      <p className='post-desc'>{item.desc}</p>
+                    </div>
+                  </div>
+                )
+              )
+            }
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default News;
