f720e13ca4f6f1f4066d52722d97648c3e3daf72
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-02-25 12:24:19 +0300

    fix: coffeeItems

--- src/pages/Catalog/Coffee/Coffee-Items/Coffee-Items.jsx
+++ src/pages/Catalog/Coffee/Coffee-Items/Coffee-Items.jsx
@@ -12,7 +12,6 @@ import roast2 from '../../../../assets/Catalog/Coffee/Filters/roast2.svg'
 import roast1 from '../../../../assets/Catalog/Coffee/Filters/roast1.svg'
 import axios from "axios";
 import { Link } from 'react-router-dom';
-
 const CoffeeItems = () => {
   const [cards, setCards] = useState([])
   useEffect(()=>{ 
@@ -48,9 +47,37 @@ const CoffeeItems = () => {
                     </div>
                   </div>
                   <div className="coffee__cards-inner-row-card-center">
-                    <Link to="/cards" className="tea-img-block">
-                      <img src={item.image} height='218px' className="card-image" alt="" />
-                    </Link>
+                    <div className="coffee__cards-inner-row-card-center-left">
+                      <Link to="">
+                        <img src={item.image} width='120px' className="card-image" alt="" />
+                      </Link>
+                    </div>
+                    <div className="coffee__cards-inner-row-card-center-right">
+                      <div className="coffee__cards-inner-row-card-center-right-rating">
+                        <div className="rating" style={{backgroundImage: `url(${rating})`, height: '12px', width: `${(Math.floor(item.rating[0]) + item.rating[2])}px`, backgroundSize: '50px', backgroundRepeat: 'no-repeat', position: 'relative'}}></div>
+                        <h4>{item.rating}</h4>
+                        <small className="coffee__cards-inner-row-card-center-right-rating-review">({item.review} отзыва)</small>
+                      </div>
+                      <div className="coffee__cards-inner-row-card-center-right-roasting">
+                        {
+                          item.roast == 5 ? <img height='13px' src={roast5} alt=""/> : item.roast == 4 ? <img height='13px' src={roast4} alt=''/> : item.roast == 3 ? <img height='13px' src={roast3} alt=''/> : item.roast == 2 ? <img height='13px' src={roast2} alt=''/> : item.roast == 1 ? <img height='13px' src={roast1} alt=''/> : null
+                        }
+                      </div>
+                      <div className="coffee__cards-inner-row-card-center-right-filters">
+                        <div className="coffee__cards-inner-row-card-center-right-filters-item">
+                          <p>Кислинка</p>
+                          <div className="kislinka"><img src={item.kislinka > 7 ? kislinkaHigh : item.kislinka > 3 ? kislinkaMedium : item.kislinka > 0 ? kislinkaLow : null } alt="" /></div>
+                        </div>
+                        <div className="coffee__cards-inner-row-card-center-right-filters-item">
+                          <p>Горчинка</p>
+                          <div className="kislinka"><img src={item.gorchinka > 7 ? kislinkaHigh : item.gorchinka > 3 ? kislinkaMedium : item.gorchinka > 0 ? kislinkaLow : null  } alt="" /></div>
+                        </div>
+                        <div className="coffee__cards-inner-row-card-center-right-filters-item">
+                          <p>Носыщенность</p>
+                          <div className="kislinka"><img src={item.nasyshennost > 7 ? kislinkaHigh : item.nasyshennost > 3 ? kislinkaMedium : item.nasyshennost > 0 ? kislinkaLow : null  } alt="" /></div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                   <div className="coffee__cards-inner-row-card-bottom">
                     <small>{item.geography}</small>
@@ -58,7 +85,7 @@ const CoffeeItems = () => {
                     <p>{item.subtitle}</p>
                     <Link to='' className="treedots">Больше</Link><>...</>
                     <div className="coffee__cards-inner-row-card-bottom-bottom">
-                      <h3>{item.prise}</h3>
+                      <h3>{item.price}</h3>
                       <button>В корзину</button>
                     </div>
                   </div>
@@ -73,5 +100,4 @@ const CoffeeItems = () => {
     </>
   );
 };
-
 export default CoffeeItems; 
