fae8773831f67d718aad6b5d09c2e18ef08ac8ee
Ruslan321532 <ruslanburalkiev8@gmail.com>
2023-02-25 11:58:37 +0600

    fix whatsapp, preloader

--- src/App.js
+++ src/App.js
@@ -28,10 +28,10 @@ function App() {
   return (
     <>
       {loading ? (
-        <div class="loader">
-          <div class="inner one"></div>
-          <div class="inner two"></div>
-          <div class="inner three"></div>
+        <div className="loader">
+          <div className="inner one"></div>
+          <div className="inner two"></div>
+          <div className="inner three"></div>
         </div>
       ) : (
         <Routes>
