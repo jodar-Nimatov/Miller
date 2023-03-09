5aa61c560bdce46145b648c2fc4010e91729540e
Kanatbek999 <115145077+Kanatbek999@users.noreply.github.com>
2023-03-09 16:34:21 +0600

    fix: profile style

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import coffee from "../../assets/Profile/coffeeProfile.png";
 import joodar from "../../assets/Profile/Joodar.png";
+import CoffeeImg from "../../assets/Profile/coffeeImgProfile1.svg"
 import { Link } from "react-router-dom";
 
 const Profile = () => {
+  const [show, setShow] = useState(true)
 
-  
   return (
     <div className="profile">
       <div className="container">
@@ -34,26 +35,34 @@ const Profile = () => {
               </ul>
             </div>
             <div className="discount__profile">
-              <div className="discount__inner">
-                <div className="discount__button">
-                  <h1 className="discount__text">Ваша скидка: 10%</h1>
-                  <Link to="#"> 
-                    <button className="discount__btn">?</button>
-                  </Link>
-                </div>
-
-                <li>Сумма заказов: 5675 ₽*</li>
-                <p className="discount__title">*До скидки 15% не хватает покупок на сумму: 1255 ₽</p>
-              </div>
+            <div className="discount__inner">
+                                    <button className="discount__btn" onClick={()=>setShow(!show)}>?</button> 
+                                <div className={`discount-block ${show ? 'block' : 'none'}`}>
+                                    <h1 className="discount__text">Ваша скидка: 10%</h1>
+                                    <li>Сумма заказов: 5675 ₽*</li>
+                                    <p>*До скидки 15% не хватает покупок на сумму: 1255 ₽</p> 
+                                   
+                                </div> 
+                                       
+                                <div className={`discount-block ${show ? 'none' : 'block'}`}>
+                                    <h1 className="discount__title">До скидки 15% не хватает покупок на сумму: 1255 ₽</h1>
+                                    <p className="discount__p">Скидка 10% - сумма покупок 5000 ₽ </p>
+                                    <p className="discount__p">Скидка 15% - сумма покупок 7000 ₽ </p>
+                                    <p className="discount__p">Скидка 20% - сумма покупок 10000 ₽ </p>
+                                </div>
+                       
+                            </div>
             </div>
           </div>
 
           <div className="orders__cart">
             <div className="orders__title">
               <h1>Мои заказы</h1>
-
-              <button>Текущие заказы</button>
+              <div className="orders__btn">
+                <button>Текущие заказы</button>
               <button>Завершенные</button>
+              </div>
+              
               <div className="orders__p">
                 <p>01.08.2021 12:24:00 - оплачено</p>
                 <p>Дата доставки: 03.08.2021</p>
@@ -62,12 +71,12 @@ const Profile = () => {
             <div className="info">
               <div className="info__text">
                 <h1>Товаров:</h1>
-                <ul>
+                <ul className="info__title">
                   <li>2 х Columbia Supremo, 250 г.</li>
-                  <li>1 х Напиток ячменый Millor, 250 г.</li>
+                  <li>2 х Columbia Supremo, 250 г.</li>
                 </ul>
               </div>
-              <div className="price__one">
+              <div>
                 <h1>Сумма заказа:</h1>
                 <ul>
                   <li>480 ₽</li>
@@ -93,10 +102,10 @@ const Profile = () => {
               <li>Сумма заказа: 864 ₽</li>
               <li>Доставка: 350 ₽ </li>
             </ul>
-          </div>
+          </div> 
 
           <div className="slider__profile">
-            <h1>Персональные акции</h1>
+            <h1 className="slider__text">Персональные акции</h1>
             <div className="slides__inner ">
               <div className="coffee__profile">
                 <img src={coffee} alt="" />
