642133b00445efae6b9289578fd36a5bee75cb21
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-03-11 21:42:59 +0300

    add: errors..

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Layout from "./Layout/Layout";
@@ -6,18 +6,24 @@ import Contacts from "./pages/Contacts/Contacts";
 import Blog from "./pages/Blog/Blog";
 import "./scss/style.scss";
 import Profile from "./pages/Profile/Profile";
-import Layolog from "./pages/Catalog/Layolog";
+import Cart from './pages/Cart/Cart'
 import Store from "./pages/Contacts/Store";
+import SignUp from "./pages/SignUp/SignUp";
+// catalog
+import VendingFilters from "./pages/Catalog/Vending/Vending-Filters/Vending-Filters";
 import CoffeeFilters from "./pages/Catalog/Coffee/Coffee-Filters/Coffee-Filters";
 import TeaFilters from "./pages/Catalog/Tea/Tea-Filters/Tea-Fiters";
-import VendingFilters from "./pages/Catalog/Vending/Vending-Filters/Vending-Filters";
 import HealthyFilters from "./pages/Catalog/HealthyEating/HealthyEating-Filters/HealthyEating-Filters";
-import SignUp from "./pages/SignUp/SignUp";
-import Cart from './pages/Cart/Cart'
-import ProductCard from "./pages/ProductCard/ProductCard";
+import ProductCard from "./pages/Catalog/ProductCard/ProductCard";
+import Layolog from "./pages/Catalog/Layolog";
+// import { CustomContext } from "./Context";
+// catalog
 
 
 function App() {
+  // const {cardid} = useContext(CustomContext)
+  // const {carditem} = useContext(CustomContext)
+  // console.log(cardid, carditem)
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
@@ -32,14 +38,11 @@ function App() {
         <Route path="/catalog/tea" element={<TeaFilters />} />
         <Route path="/catalog/wanding" element={<VendingFilters />} />
         <Route path="/catalog/healthy" element={<HealthyFilters />} />
-        <Route path="/catalog/card-item" element={<ProductCard />} />
-        {/* Каталог товаров */}
+        <Route path={"/catalog/somethingFromContext/"} element={<ProductCard />} />
         {/* Корзина */}
         <Route path='/cart' element={<Cart/>}/>
-        {/* Корзина */}
         {/* Профиль пользователя */}
         <Route path='/profile' element={<Profile/>}/>
-        {/* Профиль пользователя */}
       </Route>
     </Routes>
   );
