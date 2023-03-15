c282abea5db0565ba886683ca37c6ff75e6b2881
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-03-15 13:16:35 +0600

    доделал бургер меню

--- src/Layout/Header/BurgerSD.jsx
+++ src/Layout/Header/BurgerSD.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import millerLogo from "../../assets/Header/millerLofo.svg";
 import basketlogo from "../../assets/Header/basket.svg";
 import userlogo from "../../assets/Header/user.svg";    
+import BGtellIcon from "../../assets/Images/BGtellIcon.png"
+import BGinstaIcon from "../../assets/Images/InstagramIcon.png"
 import {TfiClose} from "react-icons/tfi"
 import { Link } from 'react-router-dom';
 
@@ -10,10 +12,12 @@ const BurgerSD = ({BurgerMenu, setBurgerMenu}) => {
             <div className="burger-menu" style={{display: BurgerMenu ? 'flex':'none'}}>
             <div className="burger-menu-inner">
               <div className="burger-menu-above">
+                <Link onClick={() => setBurgerMenu(false)} to=''>
                 <img className="burger-menu-logo" src={millerLogo} alt="" />
-                <div className="close-burger-menu" onClick={() => setBurgerMenu(false)}>
-                  <TfiClose/>
-                </div>
+                    </Link>
+                  <div className="close-burger-menu" onClick={() => setBurgerMenu(false)}>
+                    <TfiClose/>
+                  </div>
               </div>
               <div className="burger-menu-bellow">
                 <div className="burger-menu-nav-list">
@@ -26,6 +30,13 @@ const BurgerSD = ({BurgerMenu, setBurgerMenu}) => {
                 <div className="burger-menu-icons">
                   <Link onClick={() => setBurgerMenu(false)} to='/cart'><img src={basketlogo} alt="" /></Link>
                   <Link onClick={() => setBurgerMenu(false)} to='/profile'><img src={userlogo} alt="" /></Link>
+                </div>
+                <div className="burger-menu-feedback">
+                  <p>import@miller.com</p>
+                  <div className="burger-menu-feedback-icons">
+                    <Link onClick={() => setBurgerMenu(false)} to='/contacts'><img src={BGtellIcon} alt="" /></Link>
+                    <a href='https://www.instagram.com/miller_coffeeandmore/'><img src={BGinstaIcon} alt="" /></a>
+                  </div>
                 </div>
               </div>
             </div>
