472c0d6b8cee6e4f01b6763d8fb63c64c6065873
jodar-Nimatov <115215766+jodar-Nimatov@users.noreply.github.com>
2023-03-18 16:57:31 +0600

    ass

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -12,7 +12,7 @@ const Profile = () => {
   const [info, setInfo] = useState(true);
   const { logout, name, user, number } = UserAuth();
   const navigate = useNavigate();
-  const { cart, plusOneCart, minusOneCart, delCart, delTo } = useContext(CustomContext);
+  const { cart, plusOneCart, minusOneCart, delCart, delTo, adds } = useContext(CustomContext);
   const total = cart.reduce(
     (prev, curr) => prev + curr.price * curr.count - ((curr.price * curr.count) / 100) * 10,
     0,
